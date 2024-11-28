86f1cdf6b613ce4e6849a07e7bb01a0047a7021b
D_Zucho <danny.nico.dev@outlook.com>
2024-11-28 02:42:56 -0800

    add hero initial styles w/o animations

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,31 @@
-import React, {HTMLAttributes} from 'react';
+import React, {ButtonHTMLAttributes} from 'react';
 import {cva} from 'class-variance-authority'
 
-const classes = cva('border h-12 rounded-full px-6 font-medium', {
+const classes = cva('border h-12 rounded-full ' +
+	'focus:outline-none px-6 font-medium', {
 	variants: {
 		variant: {
 			primary: 'bg-lime-400 text-neutral-950 border-lime-400',
 			secondary: 'border-white text-white bg-transparent',
+		},
+		size: {
+			sm: 'h-10'
 		}
 	}
 })
 
 const Button = (props: {
-	variant: "primary" | "secondary"
-} & HTMLAttributes<HTMLButtonElement>) => {
-	const {variant, className, ...restProps} = props
+	variant: "primary" | "secondary",
+	size?: "sm";
+} & ButtonHTMLAttributes<HTMLButtonElement>) => {
+	const {variant, className, size, ...restProps} = props
 
 	return (
 		<div>
 			<button className={classes({
-				variant: props.variant,
-				className: props.className
+				variant,
+				size,
+				className
 			})}
 							{...restProps}
 			/>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,41 @@
+import Button from "@/components/Button";
+
 export default function Hero() {
-    return <div>Hero</div>;
+	return <section className={'py-24'}>
+		<div className="container">
+			<div className={'flex justify-center'}>
+
+				<div className={'inline-flex py-1 px-3 bg-gradient-to-r' +
+					' from-purple-400 to-pink-400 rounded-full text-neutral-950' +
+					' font-semibold'}>✨ $7.5M seed round raised
+				</div>
+			</div>
+			<h1 className={'text-6xl font-medium text-center mt-6 md:text-7xl'}>Impactful design, created effortlessly</h1>
+			<p className={'text-center font-xl text-white/50 mt-8 max-w-2xl mx-auto'}>Design tools shouldn&apos;t slow you down. Layers combines powerful
+				features with an intuitive interface that keeps you in your
+				creative flow
+			</p>
+
+
+
+			<form className={'flex justify-between border border-white/15' +
+				' rounded-full p-2' +
+				' mt-8 md:max-w-lg mx-auto overflow-clip'}>
+
+
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder={'Enter your email'}
+					className={'bg-transparent pl-4 focus:outline-none max-w-[200px]' +
+						' md:max-w-full'}
+				/>
+
+				<Button type="submit" size={'sm'} variant={'primary'} className={'whitespace-nowrap'}>Sign Up</Button>
+
+			</form>
+
+		</div>
+	</section>;
 }
