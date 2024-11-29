283aef1c2787343f117516a97688cab5c69217b8
D_Zucho <danny.nico.dev@outlook.com>
2024-11-29 02:03:02 -0800

    add CTA section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
-import Navbar from "@/sections/Navbar";
-import Hero from "@/sections/Hero";
-import LogoTicker from "@/sections/LogoTicker";
-import Introduction from "@/sections/Introduction";
-import Features from "@/sections/Features";
-import Integrations from "@/sections/Integrations";
+import Navbar from '@/sections/Navbar'
+import Hero from '@/sections/Hero'
+import LogoTicker from '@/sections/LogoTicker'
+import Introduction from '@/sections/Introduction'
+import Features from '@/sections/Features'
+import Integrations from '@/sections/Integrations'
+import Faqs from '@/sections/Faqs'
+import CallToAction from '@/sections/CallToAction'
 
 export default function Home() {
-    return <div>
-        <Navbar />
-        <Hero />
-        <LogoTicker />
-        <Introduction />
-        <Features />
-        <Integrations />
-    </div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <LogoTicker />
+      <Introduction />
+      <Features />
+      <Integrations />
+      <Faqs />
+      <CallToAction />
+    </div>
+  )
 }
