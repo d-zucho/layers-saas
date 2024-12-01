46020e7fd85bed099b9f7a736187b2ce4939fd6e
D_Zucho <danny.nico.dev@outlook.com>
2024-12-01 05:24:40 -0800

    add features section animations

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,106 +1,189 @@
-import Tag from "@/components/Tag";
-import FeatureCard from "@/components/FeatureCard";
+import Tag from '@/components/Tag'
+import FeatureCard from '@/components/FeatureCard'
 import avatar1 from '@/assets/images/avatar-ashwin-santiago.jpg'
 import avatar2 from '@/assets/images/avatar-lula-meyers.jpg'
 import avatar3 from '@/assets/images/avatar-florence-shaw.jpg'
-import Image from "next/image";
-import Avatar from "@/components/Avatar";
-import Key from "@/components/Key";
+import avatar4 from '@/assets/images/avatar-owen-garcia.jpg'
+import Image from 'next/image'
+import Avatar from '@/components/Avatar'
+import Key from '@/components/Key'
 
 const features = [
-	"Asset Library",
-	"Code Preview",
-	"Flow Mode",
-	"Smart Sync",
-	"Auto Layout",
-	"Fast Search",
-	"Smart Guides",
-];
+  'Asset Library',
+  'Code Preview',
+  'Flow Mode',
+  'Smart Sync',
+  'Auto Layout',
+  'Fast Search',
+  'Smart Guides',
+]
 
 export default function Features() {
-	return <section className={'py-94'}>
-		<div className={'container'}>
-			<div className={'flex justify-center'}>
-				<Tag>Features</Tag>
-			</div>
-			<h2 className={'text-6xl font-medium text-center mt-6 lg:max-w-2xl mx-auto'}>
-				Where power meets <span className={'text-lime-400'}>simplicity</span>
-			</h2>
+  return (
+    <section className={'py-94'}>
+      <div className={'container'}>
+        <div className={'flex justify-center'}>
+          <Tag>Features</Tag>
+        </div>
+        <h2
+          className={
+            'text-6xl font-medium text-center mt-6 lg:max-w-2xl mx-auto'
+          }
+        >
+          Where power meets <span className={'text-lime-400'}>simplicity</span>
+        </h2>
 
-			<div className={'mt-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6' +
-				' gap-8'}>
-				{/* Feature 1 */}
-				<FeatureCard
-					title={'Real-time Collaboration'}
-					description={'Work together seamlessly with conflict-free team editing.'}
-					className={'md:col-span-2'}
-				>
-					<div className={'aspect-video flex items-center justify-center'}>
-						<Avatar className={'z-40'}>
-							<Image src={avatar1} alt={'Avatar 1'} className={'rounded-full'}/>
-						</Avatar>
-						<Avatar className={'-ml-6 border-indigo-500 z-30'}>
-							<Image src={avatar2} alt={'Avatar 2'} className={'rounded-full'}/>
-						</Avatar>
-						<Avatar className={'-ml-6 border-amber-500 z-20'}>
-							<Image src={avatar3} alt={'Avatar 3'} className={'rounded-full'}/>
-						</Avatar>
-						<Avatar className={'-ml-6 border-transparent'}>
-							<div className={'size-full bg-neutral-700 rounded-full' +
-								' inline-flex items-center justify-center gap-1'}>
-								{Array.from({length: 3}).map((_, i) => (
-									<span key={i} className={'size-1.5 rounded-full' +
-										' bg-white inline-flex'}></span>
-								))}
+        <div
+          className={
+            'mt-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6' + ' gap-8'
+          }
+        >
+          {/* Feature 1 */}
+          <FeatureCard
+            title={'Real-time Collaboration'}
+            description={
+              'Work together seamlessly with conflict-free team editing.'
+            }
+            className={'md:col-span-2 group'}
+          >
+            <div className={'aspect-video flex items-center justify-center'}>
+              <Avatar className={'z-40'}>
+                <Image
+                  src={avatar1}
+                  alt={'Avatar 1'}
+                  className={'rounded-full'}
+                />
+              </Avatar>
+              <Avatar className={'-ml-6 border-indigo-500 z-30'}>
+                <Image
+                  src={avatar2}
+                  alt={'Avatar 2'}
+                  className={'rounded-full'}
+                />
+              </Avatar>
+              <Avatar className={'-ml-6 border-amber-500 z-20'}>
+                <Image
+                  src={avatar3}
+                  alt={'Avatar 3'}
+                  className={'rounded-full'}
+                />
+              </Avatar>
+              <Avatar
+                className={
+                  '-ml-6 border-transparent group-hover:border-green-500 transition duration-300'
+                }
+              >
+                <div
+                  className={
+                    'size-full bg-neutral-700 rounded-full' +
+                    ' inline-flex items-center justify-center gap-1 relative'
+                  }
+                >
+                  <Image
+                    src={avatar4}
+                    alt='Avatar 4'
+                    className='absolute size-full rounded-full opacity-0 group-hover:opacity-100 transition duration-300'
+                  />
+                  {Array.from({ length: 3 }).map((_, i) => (
+                    <span
+                      key={i}
+                      className={
+                        'size-1.5 rounded-full' + ' bg-white inline-flex'
+                      }
+                    ></span>
+                  ))}
+                </div>
+              </Avatar>
+            </div>
+          </FeatureCard>
 
-							</div>
-						</Avatar>
-					</div>
-				</FeatureCard>
+          {/* Feature 2 */}
+          <FeatureCard
+            title={'Interactive Prototyping'}
+            description={
+              'Engage your clients with prototypes that react to' +
+              ' user actions.'
+            }
+            className={'md:col-span-2 group'}
+          >
+            <div className={'aspect-video flex items-center justify-center'}>
+              <p
+                className={
+                  'text-4xl font-extrabold text-white/30 text-center group-hover:text-white/10 transition duration-500'
+                }
+              >
+                We&apos;ve achieved{' '}
+                <span
+                  className={
+                    'bg-gradient-to-r from-purple-400' +
+                    ' to-pink-400 bg-clip-text text-transparent relative'
+                  }
+                >
+                  <span>incredible</span>
+                  <video
+                    src={'/assets/gif-incredible.mp4'}
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    className='absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl shadow-xl opacity-0 pointer-events-none group-hover:opacity-100 transition duration-500'
+                  ></video>
+                </span>{' '}
+                growth this year
+              </p>
+            </div>
+          </FeatureCard>
 
-				{/* Feature 2 */}
-				<FeatureCard
-					title={'Interactive Prototyping'}
-					description={'Engage your clients with prototypes that react to' +
-						' user actions.'}
-					className={'md:col-span-2'}
-				>
-					<div className={'aspect-video flex items-center justify-center'}>
-						<p
-							className={'text-4xl font-extrabold text-white/30 text-center'}>We&apos;ve
-							achieved <span className={'bg-gradient-to-r from-purple-400' +
-								' to-pink-400 bg-clip-text text-transparent'}>incredible</span> growth this year</p>
-					</div>
-				</FeatureCard>
+          {/* Feature 3 */}
+          <FeatureCard
+            title={'Keyboard Quick Actions'}
+            description={
+              'Powerful commands to help you create designs more quickly.'
+            }
+            className={'md:col-span-2 md:col-start-2 lg:col-start-5 group'}
+          >
+            <div
+              className={
+                'aspect-video flex items-center justify-center' + ' gap-4'
+              }
+            >
+              <Key
+                className={
+                  'w-28 outline outline-transparent group-hover:outline-lime-400 outline-2 outline-offset-4 group-hover:translate-y-1 transition-all duration-500'
+                }
+              >
+                shift
+              </Key>
+              <Key className='outline outline-transparent group-hover:outline-lime-400  outline-2 outline-offset-4 group-hover:translate-y-1 transition-all duration-500 delay-150'>
+                alt
+              </Key>
+              <Key className='outline outline-transparent group-hover:outline-lime-400 outline-2 outline-offset-4 group-hover:translate-y-1 transition-all duration-500 delay-300'>
+                C
+              </Key>
+            </div>
+          </FeatureCard>
+        </div>
 
-				{/* Feature 3 */}
-				<FeatureCard
-					title={'Keyboard Quick Actions'}
-					description={'Powerful commands to help you create designs more quickly.'}
-					className={'md:col-span-2 md:col-start-2 lg:col-start-5'}
-				>
-					<div className={'aspect-video flex items-center justify-center' +
-						' gap-4'}>
-						<Key className={'w-28'}>shift</Key>
-						<Key>alt</Key>
-						<Key>C</Key>
-					</div>
-				</FeatureCard>
-
-			</div>
-
-			<div className={'mt-8 flex flex-wrap gap-3 justify-center'}>
-				{features.map((feature) => (
-					<div key={feature} className={'bg-neutral-900 border' +
-						' border-white/10 inline-flex px-3 py-1.5 md:p-2 rounded-2xl' +
-						' gap-3' +
-						' items-center'}>
-						<span className={'bg-lime-400 text-neutral-950 size-5' +
-							' rounded-full inline-flex items-center justify-center text-xl'}>&#10038;</span>
-						<span className={'font-medium md:text-lg'}>{feature}</span>
-					</div>
-				))}
-			</div>
-		</div>
-	</section>;
+        <div className={'mt-8 flex flex-wrap gap-3 justify-center'}>
+          {features.map((feature) => (
+            <div
+              key={feature}
+              className={
+                'bg-neutral-900 border border-white/10 inline-flex px-3 py-1.5 md:p-2 rounded-2xl gap-3 items-center group hover:scale-105 transition duration-500'
+              }
+            >
+              <span
+                className={
+                  'bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex items-center justify-center text-xl group-hover:rotate-45 transition duration-500'
+                }
+              >
+                &#10038;
+              </span>
+              <span className={'font-medium md:text-lg'}>{feature}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
